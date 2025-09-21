6133518aa8119660d9bdcf4f6e882f7368b014b1
artem <shurtem@mail.ru>
2025-09-21 23:14:08 +0300

    homework-6.1

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,9 @@
 import clsx from "clsx";
 import styles from "./HomePage.module.scss";
 import { useTheme } from "@/shared/lib/theme/useTheme";
+import { ItemList } from "@/shared/ui/ItemList/ItemList";
+import { testDataPost } from "@/shared/mocks/testDataPost";
+import { formatDate } from "@/utils/formatDate";
 
 export const HomePage = ({}: {}) => {
 	const { theme } = useTheme();
@@ -11,6 +14,17 @@ export const HomePage = ({}: {}) => {
 					<li>Переход на страницу конкретного поста осуществляется по клику на заголовок данного поста.</li>
 					<li>Переход на страницу альбома с фото осуществляется по клику на данный альбом в альбомах пользователя.</li>
 				</ul>
+
+				<ItemList
+					title="ItemList"
+					items={testDataPost}
+					children={(post) => (
+						<>
+							<p>{post.text}</p>
+							<p>{formatDate(post.date)}</p>
+						</>
+					)}
+				/>
 			</div>
 		</div>
 	);
